6d622ba2de584dbbcbea1b2f9438a6c7564df14f
AhmadNuzulurRozaq <nuzulnazil14@gmail.com>
2026-04-20 20:46:09 +0700

    Perubahan CSS

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +11,7 @@ export default defineConfig({
                 ],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 
     server: {
