8f5a30a47a36d85712962fd1662f9a10c4877a68
juve loiso novelino <juveloiso12@gmail.com>
2026-04-22 09:10:02 +0700

    Update halaman detail

--- resources/views/customer/detail.blade.php
+++ resources/views/customer/detail.blade.php
@@ -5,5 +5,8 @@
 @section('content')
 
 <h1 class="text-3xl font-bold p-4">DETAIL BARANG | JUVE</h1>
+<h1 class="text-3xl font-bold p-4">DETAIL BARANG | JUVE</h1>
+<h1 class="text-3xl font-bold p-4">DETAIL BARANG | JUVE</h1>
+<h1 class="text-3xl font-bold p-4">DETAIL BARANG | JUVE</h1>
 
 @endsection
