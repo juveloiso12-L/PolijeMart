64560391b20f2ce9ee66e7f4f640c8ef35505eff
juve loiso novelino <juveloiso12@gmail.com>
2026-04-28 08:12:01 +0700

    Update index user

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -3,5 +3,8 @@
 @section('content')
 
 <h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
+<h1 class="text-3xl font-bold p-4">HALAMAN UTAMA | NAUFAL</h1>
 
 @endsection
